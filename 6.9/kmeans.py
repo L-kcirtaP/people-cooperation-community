66da20ab989e29c2b184d15cd209a7f6c0ee545c
L-kcirtaP <116010151@link.cuhk.edu.cn>
2019-11-17 13:13:08 +0800

    add 6.9 solutiojn

--- 6.9/kmeans.py
+++ 6.9/kmeans.py
@@ -4,28 +4,20 @@ from sklearn.cluster import KMeans
 
 if __name__ == '__main__':
 
-    data = [
-        [1.33, 4.19],
-        [1.94, 5.47],
-        [1.31, 4.32],
-        [2.48, 5.64],
-        [1.84, 5.17],
-        [2.75, 6.35],
-        [1.45, 4.68],
-        [1.33, 3.96],
-        [2.63, 5.62],
-        [1.95, 5.02],
-        [1.13, 4.34],
-        [2.64, 5.64],
-        [1.86, 5.33],
-        [1.25, 3.18],
-        [1.30, 4.36],
-        [1.94, 5.39],
-    ]
+	# read data from text file
+    data_file = open('data.txt', 'r')
+    data = []
 
+    for line in data_file.readlines():
+    	data.append(list(map(float, line.strip().split(' '))))
+
+    data_file.close()
+
+    # apply K-Means algorithm to fit the data set, and get cluster centers
     kmeans = KMeans(n_clusters=3).fit(data)
     centers = kmeans.cluster_centers_
-    
+
+    # draw the scatter diagram to illustrate the result
     scatter = plt.scatter([i[0] for i in data], [i[1] for i in data], c=kmeans.predict(data), s=50)
     for i, j in centers:
         plt.scatter(i, j, s=50, c='red', marker='+')
